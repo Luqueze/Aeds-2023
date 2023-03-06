c35c55fb596105cf493d886b3da868bcc4809d02
Lucas Caldeira <93887953+Luqueze@users.noreply.github.com>
2023-03-06 11:31:21 -0300

    Update 15.c

--- LISTA 1/15.c
+++ LISTA 1/15.c
@@ -15,7 +15,7 @@
         scanf("%f", &sal);
 
         valorph = sal / 8.0; //o valor pago por hora trabalhada é 1/8 do salário mínimo
-        valorph_e = sal * 1/8.0; //o valor pago por hora extra vale 1/4 do salário mínimo
+        valorph_e = sal * 1/4.0; //o valor pago por hora extra vale 1/4 do salário mínimo
         salb = nhoras * valorph; //o salário bruto equivale ao número de horas trabalhadas vezes o valor pago por hora trabalhada
         quantia_he = nhoras_e * valorph_e; // quantia hora extra equivale nhoras multiplicado pelo valor hora extra
         sal = salb + quantia_he; //o salário a receber é a soma do salário bruto e da quantia a receber pelas horas extras
