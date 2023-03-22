0c0ca23d8f84c928975ab7472a63353e1c94c2f8
Lucas <lucashaleluia@hotmail.com>
2023-03-21 22:29:21 -0300

    code added

---
+++ LISTA 3/11.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+    int main(){
+
+        int contador5 = 0, contador = 0, i, a;
+
+
+        for ( i = 0; i < 999; i++){
+
+            do{
+
+                printf("Insira os numeros desejados: ");
+                scanf("%d", &a);
+
+                contador ++;
+
+                if(a == 5){
+                    contador5 ++;
+                }
+
+            }while(a != -1);
+                  
+
+            
+        printf("A quantidade de numeros inseridas foi %d e o 5 foi digitado %d vezes.", contador - 1, contador5);
+
+        return 0;
+    }
+}    
