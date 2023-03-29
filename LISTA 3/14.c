bcf5de4d8a4a5ed3fb726e8f4a849dede2a4e794
Lucas <lucashaleluia@hotmail.com>
2023-03-29 07:42:59 -0300

    review

--- LISTA 3/14.c
+++ LISTA 3/14.c
@@ -7,15 +7,20 @@
 
         for (int i = 2; i <= 1000; i++){
 
+            //o numero a ser testado, ja começa como sendo primo
             bool eprimo = true;
 
             for(int n = 2; n <= i/2; n++){
                 if(i % n == 0){
+
+                    //caso o numero analisado nao seja primo, eprimo = false
                     eprimo = false;
                 }
             }
 
             if(eprimo){
+
+                //se o valor de eprimo nao for alterado para falso, o contador soma uma unidade
                 contador ++;
             }      
             
