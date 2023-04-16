2f01728186a408460b33343e923301a1c39f7b39
Lucas <lucashaleluia@hotmail.com>
2023-04-15 23:37:04 -0300

    add

---
+++ LISTA 4/13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+    int main(){
+        int v[10];
+        int i, j, aux;
+
+        for(i = 0; i < 10; i++){
+            printf("Digite o %dº valor: ", i+1);
+            scanf("%d", &v[i]);
+        }
+
+        
+        
+
+    }
