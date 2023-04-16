8c9ea680bc3c453fca88041c3a828e034f7a8f32
Lucas <lucashaleluia@hotmail.com>
2023-04-16 10:43:21 -0300

    add file

--- LISTA 4/13.c
+++ LISTA 4/13.c
@@ -1,15 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 
-    int main(){
-        int v[10];
-        int i, j, aux;
+int main() {
+    
+    int tamanho_A = 10;
+    int tamanho_B = 15;
 
-        for(i = 0; i < 10; i++){
-            printf("Digite o %dº valor: ", i+1);
-            scanf("%d", &v[i]);
-        }
+    int vetor_A[tamanho_A];
+    int vetor_B[tamanho_B];
+    int vetor_C[tamanho_A + tamanho_B];
 
-        
-        
-
+    
+    printf("Digite os elementos do vetor A:\n");
+    for (int i = 0; i < tamanho_A; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &vetor_A[i]);
     }
+
+    printf("\nDigite os elementos do vetor B:\n");
+    for (int i = 0; i < tamanho_B; i++) {
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &vetor_B[i]);
+    }
+
+    for (int i = 0; i < tamanho_A; i++) {
+        vetor_C[i] = vetor_A[i];
+    }
+
+    for (int i = 0; i < tamanho_B; i++) {
+        vetor_C[tamanho_A + i] = vetor_B[i];
+    }
+
+    printf("\nVetor A: ");
+    for (int i = 0; i < tamanho_A; i++) {
+        printf("%d ", vetor_A[i]);
+    }
+
+    printf("\nVetor B: ");
+    for (int i = 0; i < tamanho_B; i++) {
+        printf("%d ", vetor_B[i]);
+    }
+
+    printf("\nVetor C: ");
+    for (int i = 0; i < tamanho_A + tamanho_B; i++) {
+        printf("%d ", vetor_C[i]);
+    }
+
+    return 0;
+}
