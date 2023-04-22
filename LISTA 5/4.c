9a2eaced357c1e7a9a4823e6f655a02539074106
Lucas <lucashaleluia@hotmail.com>
2023-04-22 13:08:21 -0300

    small corrections

--- LISTA 5/4.c
+++ LISTA 5/4.c
@@ -5,15 +5,15 @@
         int m[3][3];
         int m2[3][3];
     
-        for(int i = 0;i < 3; i++){
-            for(int j = 0;j < 3; j++){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
                 printf("Digite o valor da linha %d e coluna %d: ",i+1,j+1);
                 scanf("%d",&m[i][j]);
             }
         }
 
         int maior = m[0][0];
-        for(int i = 0; i <3; i++){
+        for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
                 if(m[i][j] > maior){
                     maior = m[i][j];
