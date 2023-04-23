a76960f6a91d5628f1358d640855d473b4b641cd
Lucas <lucashaleluia@hotmail.com>
2023-04-23 17:43:07 -0300

    add file

--- LISTA 5/5.c
+++ LISTA 5/5.c
@@ -4,17 +4,17 @@
 
         int m[3][4];
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<4;j++){
+        for(int i = 0;i < 3; i++){
+            for(int j = 0;j < 4; j++){
                 printf("Digite o valor da linha %d e coluna %d: ",i+1,j+1);
                 scanf("%d",&m[i][j]);
             }
         }
 
         //loop para somar as linhas
-        for(int i=0;i<3;i++){
-            int soma=0;
-            for(int j=0;j<4;j++){
+        for(int i = 0;i < 3; i++){
+            int soma = 0;
+            for(int j = 0;j < 4;j++){
                 soma+=m[i][j];
             }
             printf("A soma da linha %d eh: %d \n",i+1,soma);
