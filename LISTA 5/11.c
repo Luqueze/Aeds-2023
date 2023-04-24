85a430820f28a9b14bbdc1ec0e809de5c0b98b3e
Lucas <lucashaleluia@hotmail.com>
2023-04-23 22:43:49 -0300

    add file

---
+++ LISTA 5/11.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+    int main(){
+
+        int m[2][2], m2[2][2], m3[2][2];
+
+        for(int i = 0;i < 2; i++){
+            for(int j = 0;j < 2; j++){
+                printf("Digite o valor da linha %d e coluna %d: ",i+1,j+1);
+                scanf("%d",&m[i][j]);
+            }
+        }
+
+        for(int i = 0;i < 2; i++){
+            for(int j = 0;j < 2; j++){
+                printf("Digite o valor da linha %d e coluna %d: ",i+1,j+1);
+                scanf("%d",&m2[i][j]);
+            }
+        }
+
+        for(int i = 0;i < 2; i++){
+            for(int j = 0;j < 2; j++){
+                m3[i][j] = m[i][j] * m2[i][j];
+            }
+        }
+
+        for(int i = 0;i < 2; i++){
+            for(int j = 0;j < 2; j++){
+                printf("%d ",m3[i][j]);
+            }
+            printf("\n");
+        }
+
+        
+    }
