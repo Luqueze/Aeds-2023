fec7f909571cb7681a0831579b995cc91d96577c
Lucas <lucashaleluia@hotmail.com>
2023-05-01 21:52:41 -0300

    add file

--- LISTA 6/4.c
+++ LISTA 6/4.c
@@ -10,6 +10,6 @@
         x = 10;
          
         //como q é um ponteiro que referencia outro ponteiro é necessário usar dois asteriscos para acessar o valor de x
-        printf("%d", **q); 
+        printf("%p", **q); 
         return(0);  
     }
