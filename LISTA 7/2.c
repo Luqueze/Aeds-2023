84c4061d88da4aad0ee6691bc7d4b2b410e3ea57
Lucas <lucashaleluia@hotmail.com>
2023-05-10 23:03:48 -0300

    add file

--- LISTA 7/2.c
+++ LISTA 7/2.c
@@ -1,14 +1,26 @@
 #include<stdio.h>
 
+    float salMedio(int);
     int main(){
-        float sal;
-        int numfilhos;
+        int numfilhos, numpessoas;
 
-        printf("Digite o salario: ");
-        scanf("%f", &sal);
+        printf("Insira quantas pessoas tem na cidade: ");
+        scanf("%d", &numpessoas);
 
         printf("Digite o numero de filhos: ");
         scanf("%d", &numfilhos);
 
-        
+        float media = salMedio(numpessoas);
+        printf("A media de salario e: %.2f\n", media);
+   
+    }
+
+    float salMedio(int numpessoas){
+        float salario, soma = 0;
+        for(int i = 0; i < numpessoas; i++){
+            printf("Digite o salario da pessoa %d: ", i+1);
+            scanf("%f", &salario);
+            soma += salario;
+        }
+        return soma/numpessoas;
     }
