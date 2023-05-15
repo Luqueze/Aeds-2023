49797b72ea4dbbb1ccc7f60916829767cd2d6200
Lucas Caldeira <93887953+Luqueze@users.noreply.github.com>
2023-05-15 15:16:15 +0000

    add file

---
+++ LISTA 7/12.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void printArray(int n, int vetor[n]){
+    int i;
+    for(i = 0; i < n; i++){ 
+        printf("%d ",vetor[i]);
+    }
+    printf("\n");
+}
+
+float mediaArray(int n, int vetor[]){
+
+    int i;
+    float soma = 0;
+    for(i = 0; i < n; i++){ 
+        soma += vetor[i];
+    }
+    return soma/n;
+    
+}
+
+int maxArray(int n, int vetor[]){
+    int i;
+    int maior = vetor[0];
+    for(i = 0; i < n; i ++){
+        if(vetor[i] > maior)
+            maior = vetor[i];    
+    }
+    return maior;
+}
+
+int minArray(int n, int vetor[]){
+    int i;
+    int menor = vetor[0];
+    for(i = 0; i < n; i++){
+        if(vetor[i] < vetor[0])
+            menor = vetor[i];
+    }
+    return menor;
+}
+
+
+    
