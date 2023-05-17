da8088869358ed46ef42173bdcda0f33f45bb208
Lucas <lucashaleluia@hotmail.com>
2023-05-16 22:40:53 -0300

    add file

---
+++ LISTA 7/arraylib.h
@@ -0,0 +1,12 @@
+#ifndef ARRAYLIB_H
+#define ARRAYLIB_H
+
+void printArray(int n, int vetor[]);
+
+float mediaArray(int n, int vetor[]);
+
+int maxArray(int n, int vetor[]);
+
+int minArray(int n, int vetor[]);
+
+#endif
