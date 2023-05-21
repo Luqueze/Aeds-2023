e61d2ae39ae1438a756f48ddecf7a0959e61d6e3
Lucas <lucashaleluia@hotmail.com>
2023-05-21 20:13:36 -0300

    add 14.c

---
+++ LISTA 7/14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+    int main(){
+
+        int fat;
+
+        printf("Digite um numero: ");
+        scanf("%d", &fatorial);
+
+        printf("O fatorial de %d e: ", fatorial(fat));
+    }
+
+    int fatorial(int fat){
+
+    if (fat == 0)
+        return 1;
+    else if (fat < 0)
+        return 0;
+    else
+        return fat * fatorial(fat - 1);
+    }
