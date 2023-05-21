d4264997d098e7ef0c9e0cffada1889c506f9c98
Lucas <lucashaleluia@hotmail.com>
2023-05-21 20:22:42 -0300

    add 16.c

---
+++ LISTA 7/16.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+    int main(){
+
+        int i;
+
+        printf("Digite o tamanho do array: ");
+        scanf("%d", &i);
+
+        float array[i];
+
+        for(int j = 0; j < i; j++){
+            printf("Digite o valor do elemento %d: ", j + 1);
+            scanf("%f", &array[j]);
+        }
+
+        printf("A soma dos elementos do array e: %.2f", soma(array, i));
+
+
+    }
+
+    float soma(float array[], int i){
+
+        if (i == 0)
+            return 0;
+        else
+            return array[i - 1] + soma(array, i - 1);
+    }
