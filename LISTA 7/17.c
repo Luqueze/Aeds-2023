b22b4412c5e2a236b7cf43aa800bdce7437d985f
Lucas Caldeira <93887953+Luqueze@users.noreply.github.com>
2023-05-22 14:55:19 +0000

    add file

--- LISTA 7/17.c
+++ LISTA 7/17.c
@@ -1,16 +1,5 @@
 #include<stdio.h>
 
-    int main(){
-
-        int n;
-        int vetor[10] = {1, 2, 3, 4, 5, 6, 5, 8, 5 ,10};
-
-        printf("Insira o numero que deseja conferir quantas vezes repete: ");
-        scanf("%d", &n);
-
-        printf("O numero %d repete %d vezes", n, repete(vetor, n, 10));
-    }
-
     int repete(int vetor[], int n, int i){
 
         if (i == 0)
@@ -20,3 +9,16 @@
         else
             return repete(vetor, n, i - 1);
     }
+    
+    int main(){
+        
+        int n;
+        int vetor[10] = {1, 2, 3, 4, 5, 6, 5, 8, 5 ,10};
+
+        printf("Insira o numero que deseja conferir quantas vezes repete: ");
+        scanf("%d", &n);
+
+        printf("O numero %d repete %d vezes", n, repete(vetor, n, 10));
+    }
+
+    
