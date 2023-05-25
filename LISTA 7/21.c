b4e5f47b1b97afe6a81a2129bd560f8ade259e75
Lucas <lucashaleluia@hotmail.com>
2023-05-25 18:33:16 -0300

    add

--- LISTA 7/21.c
+++ LISTA 7/21.c
@@ -3,7 +3,26 @@
     int main(){
         
         int vetor[10] = {1, 2, 3, 4, 5, 6, 5, 8, 5 ,10};
-        printf("Vetor invertido: ", inverte(vetor, 10));
+        
+        for(int i = 0; i < 10; i++){
+            printf("%d ", vetor[i]);
+        }
 
+        printf("\n");
 
+        inverte(vetor, 10);
+
+        for(int i = 0; i < 10; i++){
+            printf("%d ", vetor[i]);
+        }
+
+    }
+
+    void inverte(int vetor[], int n){
+        int i, j, aux;
+        for(i = 0, j = n - 1; i < j; i++, j--){
+            aux = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = aux;
+        }
     }
