78b74839abe9e78db7b1e6611f24ffd15f2c7827
Lucas <lucashaleluia@hotmail.com>
2023-05-28 21:39:42 -0300

    add 8.c

--- LISTA 8 - string/8.c
+++ LISTA 8 - string/8.c
@@ -1,5 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
-    int main(){
-        
+void removeEspacos(char str[]) {
+    int i, j;
+
+    for (i = 0, j = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            str[j] = str[i];
+            j++;
+        }
     }
+
+    str[j] = '\0';
+}
+
+int main() {
+    char str[50];
+
+    printf("Insira uma string: ");
+    fgets(str, sizeof(str), stdin);
+
+    removeEspacos(str);
+
+    printf("String sem espacos em branco: %s\n", str);
+
+    return 0;
+}
