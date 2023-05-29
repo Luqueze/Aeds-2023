f34895556d9d0c7b0d20b6e01e5153fdd85f040c
Lucas <lucashaleluia@hotmail.com>
2023-05-28 22:06:54 -0300

    add

---
+++ LISTA 8 - string/9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+    int main(){
+
+        char str[50];
+        char str2[3];
+        
+        printf("Digite uma string: ");
+        fgets(str, 50, stdin);
+
+        
+
+
+
+
+    }
