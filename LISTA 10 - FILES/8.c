2f4f31880a3d8f98743e5acd173b1d641c41686a
Lucas <lucashaleluia@hotmail.com>
2023-06-11 19:55:32 -0300

    add

--- LISTA 10 - FILES/8.c
+++ LISTA 10 - FILES/8.c
@@ -4,12 +4,33 @@
 
         FILE *arq;
 
-        arq = fopen("arquivo.txt", "w");
+        arq = fopen("arquivo8.txt", "w");
 
         if(arq == NULL){
             printf("Erro na abertura do arquivo");
             return 0;
         }
 
+        int flag = 1;
+
+        while(flag != -1){
+            int matricula = 0;
+
+            printf("Digite a matricula: ");
+            scanf("%d", &matricula);
+
+            if(matricula == -1){
+                flag = -1;
+                break;
+            }else{
+                fprintf(arq, "Matricula: %d\n", matricula);
+
+            }
+
+            
+        }
         
+
+
+
     }
