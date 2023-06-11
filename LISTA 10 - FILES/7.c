f96d869b3b2e2402a621f6d324df069189533dd7
Lucas <lucashaleluia@hotmail.com>
2023-06-11 00:05:21 -0300

    modified

--- LISTA 10 - FILES/7.c
+++ LISTA 10 - FILES/7.c
@@ -3,11 +3,20 @@
     int main(){
 
         int n;
-
+        float somatotal = 0;
+        
         printf("Insira quntos carros a locadora possui: ");
         scanf("%d", &n);
 
+        float carros[n];
+
         for(int i = 0; i < n; i++){
             printf("Insira o valor do tipo de carro %d: ", i+1);
+            scanf("%f", &carros[i]);
+            somatotal += carros[i];
+
         }
+
+        printf("O valor total arrecadado pela locadora foi de: %.2f\n", somatotal/3.0);
+        printf("O valor arrecadado por multas foi de: %.2f\n", (somatotal/10.0) * 0.2);
     }
