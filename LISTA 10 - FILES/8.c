e8a459d1db1cb54d8857ab6b096be6f23ee4f222
Lucas <lucashaleluia@hotmail.com>
2023-06-11 22:15:42 -0300

    finish

--- LISTA 10 - FILES/8.c
+++ LISTA 10 - FILES/8.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
     int main(){
 
@@ -14,21 +15,52 @@
         int flag = 1;
 
         while(flag != -1){
-            int matricula = 0;
+            int matricula;
+            int telefone;
 
-            printf("Digite a matricula: ");
+            printf("\nDigite a matricula: ");
             scanf("%d", &matricula);
+
+            
+
 
             if(matricula == -1){
                 flag = -1;
                 break;
             }else{
+                printf("Digite o telefone: ");
+                scanf("%d", &telefone);
                 fprintf(arq, "Matricula: %d\n", matricula);
+                fprintf(arq, "Telefone: %d\n", telefone);
 
             }
 
-            
         }
+
+        fclose(arq);
+
+        arq = fopen("arquivo8.txt", "r");
+
+        if(arq == NULL){
+            printf("Erro na abertura do arquivo");
+            return 0;
+        }
+
+        int choice;
+        printf("Deseja ver os dados inseridos ? 1 - Sim | 2 - Nao\n");
+        scanf("%d", &choice);
+
+        if(choice == 1){
+            char c;
+            while((c = fgetc(arq)) != EOF){
+                printf("%c", c);
+            }
+        }
+
+        fclose(arq);
+
+        return 0;
+
         
 
 
