135127f7c63e4ccd24d1930fe964c117d36e5c5e
Lucas Caldeira <93887953+Luqueze@users.noreply.github.com>
2023-06-12 15:17:18 +0000

    add file

---
+++ LISTA 11 - Struct/5.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+
+    typedef struct{
+        int cod;
+        char nome[30];
+        float preco;
+    }produto;
+
+    void exibe(produto p);
+    void exibe2(produto p[], int cod);
+    int main(){
+
+        produto p[5];
+        int i;
+
+        for(i = 0;i < 5; i++){
+            printf("Digite o codigo do produto: ");
+            scanf("%d",&p[i].cod);
+            printf("Digite o nome do produto: ");
+            fgets(p[i].nome, 30, stdin);
+            fgets(p[i].nome, 30, stdin);
+            printf("Digite o preco do produto: ");
+            scanf("%f",&p[i].preco);
+        }
+
+        for(i = 0;i < 2; i++){
+            exibe(p[i]);
+        }
+
+        return 0;
+
+
+    }
+
+    //função que recebe os a estrutura e exibe suas informações
+    void exibe(produto p){
+
+        printf("Codigo: %d\n",p.cod);
+        printf("Nome: %s\n",p.nome);
+        printf("Preco: %.2f\n",p.preco);
+
+    }
+
+    //função que recebe as informações e exiba qual o produto
+    void exibe2(produto p[], int cod){
+
+        int i;
+
+        for(i = 0;i < 5; i++){
+            if(p[i].cod == cod){
+                printf("Codigo: %d\n",p[i].cod);
+                printf("Nome: %s\n",p[i].nome);
+                printf("Preco: %.2f\n",p[i].preco);
+            }
+        }
+
+    }
+
+
+
+        
