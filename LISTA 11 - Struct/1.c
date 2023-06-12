8a62bc6583cf1bf74939ad6b99b5aa0043192be3
Lucas Caldeira <93887953+Luqueze@users.noreply.github.com>
2023-06-12 11:12:15 -0300

    Add files via upload

---
+++ LISTA 11 - Struct/1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+    typedef struct{
+
+            char nome[30];
+            char telefone[11];
+
+            char endereço[50];
+            
+
+        }dados;
+
+    int main(){
+        
+        dados cliente1, cliente2;
+
+        printf("insira seu nome: ");
+        fgets(stdin,cliente1.nome, 30);
+
+        printf("Insira o telefone: ");
+        scanf(" %c", &cliente1.telefone);
+
+        printf("Insira o endereço: ");
+        fgets(stdin, cliente1.endereço, 50);
+
+        printf("insira seu nome: ");
+        fgets(stdin,cliente2.nome, 30);
+
+        printf("Insira o telefone: ");
+        scanf("%d", &cliente2.telefone);
+
+        printf("Insira o endereço: ");
+        fgets(stdin, cliente2.endereço, 50);
+
+
+        return 0;
+
+        
+    }
